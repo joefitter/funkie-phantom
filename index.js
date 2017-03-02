60bfacc5ad0e18b79f0c8b877b4c6960176e8e91
joefitter <hello@joefitter.com>
2017-03-02 13:48:43 +0000

    tets

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = () => {
   let server;
   return {
     start: () => {
-      return phantom.run('--webdriver=4444 --web-security=no').then((s) => {
+      return phantom.run('--webdriver=4444', '--web-security=no').then((s) => {
         server = s;
       });
     },
